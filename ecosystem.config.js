a88d0cccc6b2cbb248a4a0864d2f3ba81ed3ecf3
lifugui <fugui@tinoq.com>
2020-05-05 08:15:38 +0000

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name      : 'API',
+    script    : 'index.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '122.51.22.39',
+      ref  : 'origin/master',
+      repo : 'git@github.com:yangguangqishimi/monitor.git',
+      path : '/home/ubuntu/node-project',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
